4b31a02bd47fd17f780aebc23dc666ca0980b2cd
Fabiano Martini <166465909+topomartini@users.noreply.github.com>
2024-07-13 17:28:50 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+let formularioCompra = document.querySelector(".formulario-compra")
+
+function aparecerFormularioCompra() {
+    formularioCompra.style.left = "34%"
+}
+
+function desaparecerFormularioCompra() {
+    formularioCompra.style.left = "-34%"
+}
+
+let formularioTestdrive = document.querySelector(".formulario-testdrive")
+
+function aparecerFormularioTestdrive() {
+    formularioTestdrive.style.left = "34%"
+}
+
+function desaparecerFormularioTestdrive() {
+    formularioTestdrive.style.left = "-34%"
+}
